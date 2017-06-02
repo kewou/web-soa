fe7883c837b12cb0cbe105fc8d94d23e8a55626c
apalierne <arnaud.palierne@keendoo.com>
2017-06-02 17:25:26 +0200

    projet front refait from scatch

--- src/main/java/com/beezy/websoa/controller/AccueilController.java
+++ src/main/java/com/beezy/websoa/controller/AccueilController.java
@@ -3,10 +3,9 @@ package com.beezy.websoa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,11 +45,13 @@ public class AccueilController {
         etuService.delete( (long) id);
     }
     
-    @RequestMapping(value = "/etudiant/ajouter", method = RequestMethod.POST)
-    public String addEtudiant(@ModelAttribute("etudiant") Etudiant e) {
+    @RequestMapping(value = "/lesEtudiants/ajouter", method = RequestMethod.POST)
+    public List<Etudiant> addEtudiant(@RequestBody Etudiant e) {
     	this.etuService.addEtudiant(e);
-        return "redirect:/etudiants";
+    	return etuService.listEtudiant();
 
     }
+    
+
     
 }
---
+++ src/main/java/com/beezy/websoa/validator/UserValidator.java
@@ -0,0 +1,57 @@
+package com.beezy.websoa.validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import com.beezy.websoa.entities.Etudiant;
+import com.beezy.websoa.repository.EtudiantRepository;
+import com.beezy.websoa.services.EtudiantServices;
+
+
+public class UserValidator implements Validator{
+
+	@Override
+	public boolean supports(Class<?> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void validate(Object arg0, Errors arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	/*
+    @Autowired   
+    private EtudiantRepository etuRepo;
+
+	@Override
+	public boolean supports(Class<?> arg0) {
+		return Etudiant.class.equals(arg0);
+	}
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Etudiant etudiant = (Etudiant) o;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "Etudiantname", "NotEmpty");
+        if (etudiant.getUsername().length() < 6 || etudiant.getUsername().length() > 32) {
+            errors.rejectValue("Etudiantname", "Size.EtudiantForm.Etudiantname");
+        }
+        if (etuRepo.findByUsername(username)(etudiant.getUsername()) != null) {
+            errors.rejectValue("Etudiantname", "Duplicate.EtudiantForm.Etudiantname");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
+        if (etudiant.getPass().length() < 8 || etudiant.getPass().length() > 32) {
+            errors.rejectValue("password", "Size.EtudiantForm.password");
+        }
+
+//        if (!etudiant.getPasswordConfirm().equals(etudiant.getPass())) {
+//            errors.rejectValue("passwordConfirm", "Diff.EtudiantForm.passwordConfirm");
+//        }
+    }
+*/
+}
